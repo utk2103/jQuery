7152a349b33c2e098fb998681633f7b00d2d2da6
Utkarsh Upadhyay <118432516+utk2103@users.noreply.github.com>
2023-08-29 23:02:14 +0530

    Add files via upload

--- jQuery/index.js
+++ jQuery/index.js
@@ -24,5 +24,37 @@ $("button").text("Don't click me");
 $("button").html("<en>Heyyy <en>");
 
 // Manipulating Attributes with jQuery
+console.log($("img").attr("src"))
+$("a").attr("href", "https://www.yahoo.com");
 
- 
+// Adding event listener to jQuery
+$("h1").click(function(){
+    $("h1").css("color", "purple")
+})
+
+  //using javascript
+// for (var i = 0; i<5; i++) {
+//     document.querySelectorAll("button")[i].addEventListener("click", function(){
+//         document.querySelector("h1").style.color = "purple";
+//     })
+// }
+
+    //using jQuery
+$("button").click(function() {
+    $("h1").css("color", "Red");
+})
+
+  // on keypress
+//  $("document").keypress(function(event) {
+//     console.log(event.key);
+//  }); 
+
+$("h1").on("mouseover", function(){
+    $("h1").css("color", "green");
+})
+
+//Adding and removing elements -> Do have a look at syntax of below code 
+// $("h1").before(<button>New</button>);
+// $("h1").after(Heyyaaa);
+// $("h1").prepend(content); 
+// $("h1").append(content);
