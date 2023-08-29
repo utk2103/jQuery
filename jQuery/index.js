e2d1c382bf1dea8e38f621c7b222b64c8bd92a16
Utkarsh Upadhyay <118432516+utk2103@users.noreply.github.com>
2023-08-29 20:23:40 +0530

    Add files via upload

---
+++ jQuery/index.js
@@ -0,0 +1,28 @@
+//alert("Working")
+
+// $(document).ready(function(){      //used to insure if our doc is ready then it'll incoporate
+//     $("h1").css("color", "red");
+// });
+
+
+/***
+ * Selecting element in jQuery
+ * You can select one element or multiple it doesn't matter
+ * $("button"); or $("h1"); 
+ */
+
+
+/***
+ * Manipulating styles using jQuery
+ * $("h1").css("color", "green");
+ */
+$("h1").addClass("big-title margin-50");
+
+
+// Manipulating text with jQuery
+$("button").text("Don't click me");
+$("button").html("<en>Heyyy <en>");
+
+// Manipulating Attributes with jQuery
+
+ 
